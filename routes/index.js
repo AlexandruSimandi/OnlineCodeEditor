c290abe5818790abdf58d68143fe0ed0be6a7cd2
RobertSandu <robert-stelian.sandu@my.fmi.unibuc.ro>
2016-03-11 23:38:57 +0200

    added manageable nested routes

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by rober on 11/03/2016.
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html")
+});
+
+/* EXPORT the index route */
+module.exports = router;
