f33ead44424cd471cbe631a176997dfc4f448faf
RobertSandu <robert-stelian.sandu@my.fmi.unibuc.ro>
2016-03-12 12:42:17 +0200

    installed and configured bower to manage front end dependencies we will use npm install just for backend dependencies installed and configured gulp added gulp task to copy frontend dependencies to the public folder moves partial views tot the public folder

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+/**
+ * Created by rober on 12/03/2016.
+ */
+var gulp = require('gulp');
+var notify = require("gulp-notify");
+var bower = require('gulp-bower');
+
+//config object that will hold various settings
+var config  = {
+    bowerDir: './bower_components'
+}
+
+gulp.task('javascript',function(){
+    gulp
+        .src(config.bowerDir + '/jquery/dist/jquery.min.js')
+        .pipe(gulp.dest('./public/js'));
+
+    gulp
+        .src(config.bowerDir + '/angular/angular.min.js')
+        .pipe(gulp.dest('./public/js'));
+
+    gulp
+        .src(config.bowerDir + '/angular-ui-router/release/angular-ui-router.min.js')
+        .pipe(gulp.dest('./public/js'));
+
+    gulp
+        .src(config.bowerDir + '/angular-materialize/src/angular-materialize.js')
+        .pipe(gulp.dest('./public/js'));
+});
+
+
+
+gulp.task('icons', function(){
+
+});
