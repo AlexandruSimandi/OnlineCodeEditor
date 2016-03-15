5be24e21f05c0a0d843f9628c2010fd8e6930765
RobertSandu <robert-stelian.sandu@my.fmi.unibuc.ro>
2016-03-15 09:27:21 +0200

    added ace editor (cloud 9 editor) to bower dependencies and configured gulp to copy al the files to public/js/ace while maintaining the dir structure added an angular directive for ace editor but we will probably implement our own

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ var bower = require('gulp-bower');
 
 //config object that will hold various settings
 var config  = {
-    bowerDir: './bower_components'
+    bowerDir: './bower_components',
+    nodeModulesDir: './node_modules'
 }
 
 gulp.task('javascript',function(){
@@ -26,6 +27,12 @@ gulp.task('javascript',function(){
     gulp
         .src(config.bowerDir + '/angular-materialize/src/angular-materialize.js')
         .pipe(gulp.dest('./public/js'));
+    gulp
+        .src(config.bowerDir + '/angular-ui-ace/ui-ace.min.js')
+        .pipe(gulp.dest('./public/js'));
+    gulp
+        .src(config.nodeModulesDir + '/ace-editor-builds/src/**/*')
+        .pipe(gulp.dest('./public/js/ace'));
 });
 
 
