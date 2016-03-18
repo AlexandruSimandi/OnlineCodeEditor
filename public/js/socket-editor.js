745f75bbfc9259963b39995a82c5f02dba7b5c88
Alexandru <alexsimi66@gmail.com>
2016-03-18 19:25:08 +0200

    added diff tool to help sync text

--- public/js/socket-editor.js
+++ public/js/socket-editor.js
@@ -7,9 +7,13 @@ $(document).ready(function(){
 			socket.emit('text change', editor.getValue());
 		})
 
+		//replace result with msg to get back to old version
 		socket.on('text change', function(msg){
+			var oldText = editor.getValue();
+			var newText = msg;
+			var result =  diffString(oldText, newText).replace(/(<del[\s\S]*?<\/del>)|<ins>|<\/ins>/gm,'');
 			var cursor = editor.getCursorPosition();
-			editor.setValue(msg);
+			editor.setValue(result);
 			editor.gotoLine(cursor.row+1,cursor.column,false);
 		});
 	}, 200);
