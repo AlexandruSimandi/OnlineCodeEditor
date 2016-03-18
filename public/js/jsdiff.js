48c31acf2e66164cbc3b6273e86c230ffbe8eb99
Alexandru Simandi <alex_simi_jr@yahoo.com>
2016-03-18 23:17:07 +0200

    trying to dance with jsdiff

--- public/js/jsdiff.js
+++ public/js/jsdiff.js
@@ -42,22 +42,26 @@ function diffString( o, n ) {
     if (out.n.length == 0) {
         for (var i = 0; i < out.o.length; i++) {
             str += '<del>' + escape(out.o[i]) + oSpace[i] + "</del>";
+//            str += '<del>' + escape(out.o[i]) + "</del>";
         }
     } else {
         if (out.n[0].text == null) {
             for (n = 0; n < out.o.length && out.o[n].text == null; n++) {
                 str += '<del>' + escape(out.o[n]) + oSpace[n] + "</del>";
+//                str += '<del>' + escape(out.o[n]) + "</del>";
             }
         }
 
         for ( var i = 0; i < out.n.length; i++ ) {
             if (out.n[i].text == null) {
                 str += '<ins>' + escape(out.n[i]) + nSpace[i] + "</ins>";
+//                str += '<ins>' + escape(out.n[i]) + "</ins>";
             } else {
                 var pre = "";
 
                 for (n = out.n[i].row + 1; n < out.o.length && out.o[n].text == null; n++ ) {
                     pre += '<del>' + escape(out.o[n]) + oSpace[n] + "</del>";
+//                    pre += '<del>' + escape(out.o[n]) + "</del>";
                 }
                 //aici era un spatiu
                 str += "" + out.n[i].text + nSpace[i] + pre;
@@ -66,56 +70,6 @@ function diffString( o, n ) {
     }
 
     return str;
-}
-
-function randomColor() {
-    return "rgb(" + (Math.random() * 100) + "%, " +
-        (Math.random() * 100) + "%, " +
-        (Math.random() * 100) + "%)";
-}
-function diffString2( o, n ) {
-    o = o.replace(/\s+$/, '');
-    n = n.replace(/\s+$/, '');
-
-    var out = diff(o == "" ? [] : o.split(/\s+/), n == "" ? [] : n.split(/\s+/) );
-
-    var oSpace = o.match(/\s+/g);
-    if (oSpace == null) {
-        oSpace = ["\n"];
-    } else {
-        oSpace.push("\n");
-    }
-    var nSpace = n.match(/\s+/g);
-    if (nSpace == null) {
-        nSpace = ["\n"];
-    } else {
-        nSpace.push("\n");
-    }
-
-    var os = "";
-    var colors = new Array();
-    for (var i = 0; i < out.o.length; i++) {
-        colors[i] = randomColor();
-
-        if (out.o[i].text != null) {
-            os += '<span style="background-color: ' +colors[i]+ '">' +
-                escape(out.o[i].text) + oSpace[i] + "</span>";
-        } else {
-            os += "<del>" + escape(out.o[i]) + oSpace[i] + "</del>";
-        }
-    }
-
-    var ns = "";
-    for (var i = 0; i < out.n.length; i++) {
-        if (out.n[i].text != null) {
-            ns += '<span style="background-color: ' +colors[out.n[i].row]+ '">' +
-                escape(out.n[i].text) + nSpace[i] + "</span>";
-        } else {
-            ns += "<ins>" + escape(out.n[i]) + nSpace[i] + "</ins>";
-        }
-    }
-
-    return { o : os , n : ns };
 }
 
 function diff( o, n ) {
