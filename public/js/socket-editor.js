151ea8c7363656decf7d34579df0af69fa65ef99
Alexandru Simandi <alex_simi_jr@yahoo.com>
2016-03-26 10:00:40 +0200

    removed unused code and commented logs

--- public/js/socket-editor.js
+++ public/js/socket-editor.js
@@ -1,9 +1,7 @@
-//it's global for quick tests in console
-var editor
 $(document).ready(function(){
 //	timeout for angular to load, have to fix this
 	setTimeout(function(){
-
+		var editor
 		var textarea = document.getElementById('codemirror');
 		editor = CodeMirror.fromTextArea(textarea, {
 			lineNumbers: true,
@@ -14,14 +12,15 @@ $(document).ready(function(){
 		var roomName = window.location.pathname.substring(1, window.location.pathname.length);
 		socket.emit('subscribe', roomName);
 
+		//database updated every 3 secs
 		setInterval(function(){
-			console.log('database sent');
+//			console.log('database sent');
 			var text = editor.get
 			socket.emit('update room in database', {
 				roomName: roomName,
 				roomText: editor.getDoc().getValue()
 			});
-		},6000);
+		},3000);
 
 		editor.on('change', function(editor, event){
 			socket.emit('text change', {
@@ -77,71 +76,9 @@ $(document).ready(function(){
 						line: obj.roomEvent.to.line, ch: obj.roomEvent.to.ch
 					});
 					break;
-	//			editor.getDoc().replaceRange('c',{line: 0, ch: 0},{line: 0, ch: 5})
 			};
 		});
 	},100);
-//	var oldCursorRow = 1;
-//	var Range = require('ace/range').Range;
-
-//	setTimeout(function(){
-//
-//		var editor = ace.edit("editor");
-//		//updates old cursor position on click
-//		$(editor).on('click', function(){
-//			oldCursorRow = editor.getCursorPosition().row;
-//		});
-//
-//		//key pressed
-//		$('.ace_text-input').on('keyup', function(e){
-//			var cursor = editor.getCursorPosition();
-//
-//			//row deleted
-//			if(cursor.row < oldCursorRow && e.keyCode == 8){
-//				socket.emit('delete row', {
-//					roomName: roomName,
-//					roomText: oldCursorRow
-//				});
-//			}
-//			var msg = editor.session.getLine(cursor.row);
-//			msg = cursor.row +  '~~~~~' + msg + ' ';
-//			if(e.keyCode == 13){
-//				msg += '\n';
-//			}
-//			console.log(msg);
-//			socket.emit('text change', {
-//				roomName: roomName,
-//				roomText: msg
-////				roomText: editor.getValue()
-//			});
-//			socket.emit('update room in database', {
-//				roomName: roomName,
-//				roomText: editor.getValue()
-//			})
-//			oldCursorRow = cursor.row;
-//		})
-//
-//		//replace result with msg to get back to old version
-//		socket.on('text change', function(obj){
-//			var cursor = editor.getCursorPosition();
-//			var tokens = obj.roomText.split('~~~~~');
-//			console.log(tokens);
-//			var row = tokens[0];
-//			var rng = new Range(row, 0, row, Number.MAX_VALUE);
-//			try{
-//				editor.getSession().replace(rng, tokens[1]);
-//			} catch(e){}
-////			var result =  diffString(oldText, newText).replace(/(<del[\s\S]*?<\/del>)|<ins>|<\/ins>/gm,'');
-//			editor.gotoLine(cursor.row+1,cursor.column,false);
-//		});
-//
-//		socket.on('delete row', function(msg){
-//			var rng = new Range(msg.roomText, 0, msg.roomText, Number.MAX_VALUE);
-//			editor.getSession().remove(msg.roomText, msg.roomText);
-//			console.log('he deleted a row!');
-//		});
-//
-//	}, 200);
 
 });
 
