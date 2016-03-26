eb0ca71dade4f84cd942777656af07b9b6f5c48f
Alexandru Simandi <alex_simi_jr@yahoo.com>
2016-03-26 09:48:26 +0200

    text is once again saved and loaded using database

--- public/js/socket-editor.js
+++ public/js/socket-editor.js
@@ -1,73 +1,86 @@
+//it's global for quick tests in console
 var editor
 $(document).ready(function(){
-	var textarea = document.getElementById('codemirror');
-	editor = CodeMirror.fromTextArea(textarea, {
-		lineNumbers: true,
-		mode: 'javascript'
-	});
+//	timeout for angular to load, have to fix this
+	setTimeout(function(){
 
-	var socket = io();
-	var roomName = window.location.pathname.substring(1, window.location.pathname.length);
-	socket.emit('subscribe', roomName);
-
-	editor.on('change', function(editor, event){
-		socket.emit('text change', {
-			roomName: roomName,
-			roomEvent: event
+		var textarea = document.getElementById('codemirror');
+		editor = CodeMirror.fromTextArea(textarea, {
+			lineNumbers: true,
+			mode: 'javascript'
 		});
-	});
 
-	socket.on('text change', function(obj){
-		console.log(obj.roomEvent.from);
-		console.log(obj.roomEvent.to);
-		console.log(obj.roomEvent.text);
-		console.log(obj.roomEvent.removed);
-		console.log(obj.roomEvent.origin);
-		switch(obj.roomEvent.origin){
-			case('+input'):
-				var newlineFlag = '';
-				if(obj.roomEvent.text.length > 1){
-					newlineFlag = '\n';
-				}
-				editor.getDoc().replaceRange(obj.roomEvent.text[0] + newlineFlag,{
-					line: obj.roomEvent.from.line, ch: obj.roomEvent.from.ch
-				},{
-					line: obj.roomEvent.to.line, ch: obj.roomEvent.to.ch
-				});
-				break;
-			case('+delete'):
-				editor.getDoc().replaceRange('',{
-					line: obj.roomEvent.from.line, ch: obj.roomEvent.from.ch
-				},{
-					line: obj.roomEvent.to.line, ch: obj.roomEvent.to.ch
-				});
-				break;
-			case('cut'):
-				editor.getDoc().replaceRange('',{
-					line: obj.roomEvent.from.line, ch: obj.roomEvent.from.ch
-				},{
-					line: obj.roomEvent.to.line, ch: obj.roomEvent.to.ch
-				});
-				break;
-			case('paste'):
-				var str = '';
-				for(var i = 0; i < obj.roomEvent.text.length; i++){
-					str += obj.roomEvent.text[i];
-//					console.log(obj.roomEvent.text[i]);
-					if(i < obj.roomEvent.text.length - 1){
-						str += '\n';
+		var socket = io();
+		var roomName = window.location.pathname.substring(1, window.location.pathname.length);
+		socket.emit('subscribe', roomName);
+
+		setInterval(function(){
+			console.log('database sent');
+			var text = editor.get
+			socket.emit('update room in database', {
+				roomName: roomName,
+				roomText: editor.getDoc().getValue()
+			});
+		},6000);
+
+		editor.on('change', function(editor, event){
+			socket.emit('text change', {
+				roomName: roomName,
+				roomEvent: event
+			});
+		});
+
+		socket.on('text change', function(obj){
+	//		console.log(obj.roomEvent.from);
+	//		console.log(obj.roomEvent.to);
+	//		console.log(obj.roomEvent.text);
+	//		console.log(obj.roomEvent.removed);
+	//		console.log(obj.roomEvent.origin);
+			switch(obj.roomEvent.origin){
+				case('+input'):
+					var newlineFlag = '';
+					if(obj.roomEvent.text.length > 1){
+						newlineFlag = '\n';
 					}
-				}
-				console.log(str);
-				editor.getDoc().replaceRange(str, {
-					line: obj.roomEvent.from.line, ch: obj.roomEvent.from.ch
-				},{
-					line: obj.roomEvent.to.line, ch: obj.roomEvent.to.ch
-				});
-				break;
-//			editor.getDoc().replaceRange('c',{line: 0, ch: 0},{line: 0, ch: 5})
-		};
-	})
+					editor.getDoc().replaceRange(obj.roomEvent.text[0] + newlineFlag,{
+						line: obj.roomEvent.from.line, ch: obj.roomEvent.from.ch
+					},{
+						line: obj.roomEvent.to.line, ch: obj.roomEvent.to.ch
+					});
+					break;
+				case('+delete'):
+					editor.getDoc().replaceRange('',{
+						line: obj.roomEvent.from.line, ch: obj.roomEvent.from.ch
+					},{
+						line: obj.roomEvent.to.line, ch: obj.roomEvent.to.ch
+					});
+					break;
+				case('cut'):
+					editor.getDoc().replaceRange('',{
+						line: obj.roomEvent.from.line, ch: obj.roomEvent.from.ch
+					},{
+						line: obj.roomEvent.to.line, ch: obj.roomEvent.to.ch
+					});
+					break;
+				case('paste'):
+					var str = '';
+					for(var i = 0; i < obj.roomEvent.text.length; i++){
+						str += obj.roomEvent.text[i];
+						if(i < obj.roomEvent.text.length - 1){
+							str += '\n';
+						}
+					}
+					console.log(str);
+					editor.getDoc().replaceRange(str, {
+						line: obj.roomEvent.from.line, ch: obj.roomEvent.from.ch
+					},{
+						line: obj.roomEvent.to.line, ch: obj.roomEvent.to.ch
+					});
+					break;
+	//			editor.getDoc().replaceRange('c',{line: 0, ch: 0},{line: 0, ch: 5})
+			};
+		});
+	},100);
 //	var oldCursorRow = 1;
 //	var Range = require('ace/range').Range;
 
