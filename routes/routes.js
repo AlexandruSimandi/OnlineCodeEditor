af263ddfbc0d57ef28c48daaae94e9444fffa59c
RobertSandu <robert-stelian.sandu@my.fmi.unibuc.ro>
2016-07-03 15:39:20 +0300

    Using codeeditor subdomain 6

--- routes/routes.js
+++ routes/routes.js
@@ -6,13 +6,14 @@ var express = require('express'),
     router = express.Router(),
     home = require('../controllers/home'),
     api_editorData = require('../controllers/api/editorData'),
-    download = require('../controllers/download'),
-    subdomain = require('express-subdomain');
+    download = require('../controllers/download');
+
 module.exports = function(app) {
-    router.get('/:room', home.index);
-    router.get('/', home.startRoom);
-    router.get('/api/editorData/:room', api_editorData.index);
-    router.get('/download/:roomName', download.index);
+
+    router.get('/codeeditor/:room', home.index);
+    router.get('/codeeditor/', home.startRoom);
+    router.get('/codeeditor/api/editorData/:room', api_editorData.index);
+    router.get('/codeeditor/download/:roomName', download.index);
     app.use(router);
-    app.use(subdomain('codeeditor', router));
+
 };
